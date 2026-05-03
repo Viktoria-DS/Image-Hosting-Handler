396d068e56239af0db0a4599eee4f8da2be098bc
Viktoria <viktoriia.vorobyeva@fmail.com>
2026-05-03 10:06:52 +0200

    fox: restart app on fail

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from app.image_hosting_handler import ImageHostingHandler
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='[%(asctime)s] - %(name)s - %(levelname)s: %(message)s',
     datefmt='%d-%b-%y %H:%M:%S',
     handlers=[
         logging.StreamHandler(),
